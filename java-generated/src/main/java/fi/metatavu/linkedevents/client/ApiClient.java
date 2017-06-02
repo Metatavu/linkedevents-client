0d011fcb14759c9b84d4649c2c128456317a1915
Antti Leppä <antti.leppa@metatavu.fi>
2017-06-02 10:02:38 +0300

    Generated source

---
+++ java-generated/src/main/java/fi/metatavu/linkedevents/client/ApiClient.java
@@ -0,0 +1,17 @@
+package fi.metatavu.linkedevents.client;
+
+import java.util.Map;
+
+public interface ApiClient {
+  
+  public <T> ApiResponse<T> doGETRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
+  public <T> ApiResponse<T> doHEADRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
+  public <T> ApiResponse<T> doPOSTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
+  public <T> ApiResponse<T> doPUTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
+  public <T> ApiResponse<T> doDELETERequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
+}
